b271c3a9908b9dc91dd1f3f8e391f72520d8304b
clayton@xdevel.com.br <clayton@xdevel.com.br>
2015-08-26 08:09:41 -0300

    adjust worker with environment inside

--- worker.py
+++ worker.py
@@ -5,6 +5,14 @@ import sys
 def filterbyvalue(seq, value):
    for el in seq:
        if value in el: yield el
+#limitation on chronos, set environment on py
+os.environ["AWS_ACCESS_KEY_ID"]= sys.argv[2]
+os.environ["AWS_SECRET_ACCESS_KEY"]= sys.argv[3]
+os.environ["BUCKET_NAME"]=sys.argv[4]
+os.environ["AWS_DEFAULT_REGION"]=sys.argv[5]
+
+
+
 
 def main():
     print('download init')
