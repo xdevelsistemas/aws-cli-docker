956d45d8d5c438470814869b465b08002bbe771a
clayton@xdevel.com.br <clayton@xdevel.com.br>
2015-08-26 06:04:27 -0300

    ajustado uma nova tentativa de executar comando de integracao

--- worker.py
+++ worker.py
@@ -9,16 +9,16 @@ def filterbyvalue(seq, value):
 def main():
     print('download init')
     #part1 get file from s3 bucket
-    __exec = call(['aws','s3','cp',str('s3://' + os.environ['BUCKET_NAME'] + '/' + sys.argv[1] + '.jar'),'.'])
-    if (__exec == 0):
-        print('integration init')
-        #part 2 execute with pattern
-        destdir = '.'
-        for file in list(filterbyvalue([ f for f in os.listdir(destdir) if os.path.isfile(os.path.join(destdir,f)) ],sys.argv[1]))[:1]:
-            #do commands
-            call(["java","-jar"] + sys.argv[2:] + [file])
-    else:
-        raise NameError('Problem download job')
+    __exec = call(['aws','s3','cp',str('s3://' + os.environ['BUCKET_NAME'] + '/' + os.environ['JAR_FILE']),'.'])
+    # if (__exec == 0):
+    #     print('integration init')
+    #     #part 2 execute with pattern
+    #     destdir = '.'
+    #     for file in list(filterbyvalue([ f for f in os.listdir(destdir) if os.path.isfile(os.path.join(destdir,f)) ],sys.argv[1]))[:1]:
+    #         #do commands
+    #         call(["java","-jar"] + sys.argv[2:] + [file])
+    # else:
+    #     raise NameError('Problem download job')
 
 
 
