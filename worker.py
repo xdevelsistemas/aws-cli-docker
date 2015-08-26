20128a25b8aadc9faaaa1047ccd7424c190d45d8
clayton@xdevel.com.br <clayton@xdevel.com.br>
2015-08-26 04:02:08 -0300

    ajustado a ordem do comando java -jar

--- worker.py
+++ worker.py
@@ -7,16 +7,16 @@ def filterbyvalue(seq, value):
        if value in el: yield el
 
 def main():
-    print('iniciando download')
+    print('download init')
     #part1 get file from s3 bucket
     __exec = call(['aws','s3','cp',str('s3://' + os.environ['BUCKET_NAME'] + '/' + sys.argv[1] + '.jar'),'.'])
     if (__exec == 0):
-        print('download concluido buscando o componente para execucao')
+        print('integration init')
         #part 2 execute with pattern
         destdir = '.'
         for file in list(filterbyvalue([ f for f in os.listdir(destdir) if os.path.isfile(os.path.join(destdir,f)) ],sys.argv[1]))[:1]:
             #do commands
-            call(["java","-jar"] + [file] + sys.argv[2:])
+            call(["java","-jar"] + sys.argv[2:] + [file])
     else:
         raise NameError('Problem download job')
 
