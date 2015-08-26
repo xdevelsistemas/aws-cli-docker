5ee368c15957fc51663bf463ab5c9690b3508605
clayton@xdevel.com.br <clayton@xdevel.com.br>
2015-08-26 02:37:33 -0300

    alterado script do worker que estava incorreto

--- worker.py
+++ worker.py
@@ -7,14 +7,16 @@ def filterbyvalue(seq, value):
        if value in el: yield el
 
 def main():
+    print('iniciando download')
     #part1 get file from s3 bucket
     __exec = 0 #call(['aws','s3','cp',str('s3://' + os.environ['BUCKET_NAME'] + '/' + sys.argv[1] + '.jar'),'.'])
     if (__exec == 0):
+        print('download concluido buscando o componente para execução')
         #part 2 execute with pattern
         destdir = '.'
         for file in list(filterbyvalue([ f for f in os.listdir(destdir) if os.path.isfile(os.path.join(destdir,f)) ],sys.argv[1]))[:1]:
             #do commands
-            call(["java","-jar"] + [file])
+            call(["java","-jar"] + [file] + sys.argv[2:])
     else:
         raise NameError('Problem download job')
 
